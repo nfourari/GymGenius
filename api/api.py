fd919e8f9548cdc52d991203afcf2e855b425b02
adryelgainza <adryelgainza@outlook.com>
2024-09-28 14:34:06 -0400

    Personalizer AI Implementation

--- api/api.py
+++ api/api.py
@@ -7,10 +7,12 @@ class API:
         self.rspn_handler = gg_Handler()
     
     def send_request(self, req_type, crafted_request):
+        print(crafted_request)
         response = self.client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
                 {"role": "user", "content": crafted_request}
             ]
         )
+
         return self.rspn_handler.give(req_type, response)
