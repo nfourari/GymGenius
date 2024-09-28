3c4cf06e3fa4b3ca9d19b2688b5d360a35341da6
ZayWill21 <156355996+ZayWill21@users.noreply.github.com>
2024-09-28 11:37:02 -0400

    server file

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_frontend
+from main import main
 
 app = Flask(__name__)
 
 
+
 # Route to connect the homepage
 def home():
-   return render_frontend('')
+   return render_frontend('/frontend/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
